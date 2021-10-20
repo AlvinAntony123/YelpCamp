ffda051839b79d325926440c5e88496d30c5fa83
Alvin Antony <aaalvinantony@gmail.com>
2021-10-20 23:57:56 +0530

    changed name

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const helmet = require('helmet');
 
 const MongoDBStore = require("connect-mongo");
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 const campgroundRouter = require('./routes/campgrounds');
 const reviewRouter = require('./routes/reviews');
